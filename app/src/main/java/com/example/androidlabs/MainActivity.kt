d18e9c4f185b34dfc8e82db3af33da7068697229
Saja02 <tsaranja@gmail.com>
2019-09-23 19:38:33 -0400

    Added 3 layouts for Lab 2

--- app/src/main/java/com/example/androidlabs/MainActivity.kt
+++ app/src/main/java/com/example/androidlabs/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.androidlabs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CompoundButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main_linear)
+        setContentView(R.layout.activity_main_grid)
+//        setContentView(R.layout.activity_main_relative)
+
+
     }
+
+
 }
